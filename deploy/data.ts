bd70184842d39740cad091107fdae6cd0197719f
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2023-01-06 19:57:02 +0100

    SCRIPT - ADD : Added file with data used in scripts

---
+++ deploy/data.ts
@@ -0,0 +1,5 @@
+export const counterSenderAddress: string = "";
+export const counterReceiverAddress: string = "";
+export const ambSenderAddress: string = "";
+export const ambReceiverAddress: string = "";
+export const relayerAddress: string = "";
